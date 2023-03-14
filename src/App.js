2883dcec35a79dd778550aa9057f0a574bc722d0
choeminseung <choeminseung@gmail.com>
2023-03-14 17:30:23 +0900

    저장

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
+import { StatusBar, Dimensions } from 'react-native';
+import Input from './components/input';
+import { images } from './images';
+import IconButton from './components/IconButton';
+import Task from './components/Task';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppLoadingProps } from 'expo-app-loading';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+  width: ${Dimensions.get('window').width - 40}px;
+  font-size: 40px;
+  background-color: ${({ theme }) => theme.itemBackground};
+  font-weight: 600;
+  color: white;
+  border-radius: 10px;
+  text-align: center;
+`;
+
+const List = styled.ScrollView`
+  flex: 1;
+  width: ${({ width }) => width - 40}px;
+`;
+
+export default function App() {
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState({
+    1: { id: '1', text: 'React Native', completed: true },
+    2: { id: '2', text: '세계여행', completed: false },
+    3: { id: '3', text: '도서 1000권읽기', completed: false },
+  });
+
+  const _saveTasks = async tasks => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+      setTasks(tasks);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const _addTask = () => {
+    const ID = Date.now().toString();
+    const newTaskObject = {
+      [ID]: { id: ID, text: newTask, completed: false },
+    };
+    setNewTask('');
+    _saveTasks({ ...tasks, ...newTaskObject });
+  };
+
+  const _deleteTask = id => {
+    const currentTasks = Object.assign({}, tasks);
+    delete currentTasks[id];
+    _saveTasks(currentTasks);
+  };
+
+  const _toggleTask = id => {
+    const currentTasks = Object.assign({}, tasks);
+    currentTasks[id]['completed'] = !currentTasks[id]['completed'];
+    _saveTasks(currentTasks);
+  };
+
+  const _updateTask = item => {
+    const currentTasks = Object.assign({}, tasks);
+    currentTasks[item.id] = item;
+    _saveTasks(currentTasks);
+  };
+
+  const _handleTextChange = text => {
+    setNewTask(text);
+  };
+  const width = Dimensions.get('window').width;
+
+  const _onBlur = () => {
+    setNewTask('');
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={theme.background}
+        />
+        <Title>버킷 리스트</Title>
+        <Input
+          placeholder="+항목 추가"
+          value={newTask}
+          onChangeText={_handleTextChange}
+          onSubmitEditing={_addTask}
+        />
+        <List width={width}>
+          {Object.values(tasks)
+            .reverse()
+            .map(item => (
+              <Task
+                key={item.id}
+                item={item}
+                deleteTask={_deleteTask}
+                toggleTask={_toggleTask}
+                updateTask={_updateTask}
+                onBlur={_onBlur}
+              />
+            ))}
+        </List>
+      </Container>
+    </ThemeProvider>
+  );
+}
---
+++ src/images.js
@@ -0,0 +1,11 @@
+import check_box_outline from '../assets/icons/check_box_outline.png';
+import check_box from '../assets/icons/check_box.png';
+import deleteforever from '../assets/icons/deleteforever.png';
+import edit from '../assets/icons/edit.png';
+
+export const images = {
+  uncompleted: check_box_outline,
+  completed: check_box,
+  delete: deleteforever,
+  update: edit,
+};
